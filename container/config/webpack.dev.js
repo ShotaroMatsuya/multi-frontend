b52ee899b1541c74ba6f47b05e972dcfdae45720
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-02-27 13:32:44 +0900

    delegating shared modules

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -17,8 +18,8 @@ const devConfig = {
       name: 'container',
       remotes: {
         marketing: 'marketing@http://localhost:81/remoteEntry.js',
-        shared: ['react', 'react-dom'],
       },
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
