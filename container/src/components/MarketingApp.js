2dc4668e5ef114cc16e807d5e07e4397bb13baf7
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-03-12 13:25:47 +0900

    implement multi tier navigation

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,32 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  // this history object is the history object that is currently being used inside of our container.
+  // in this case , it is a copy of a browser history.
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      // Marketingのリンクを押下したときに実行される
+      // Marketing内のmemory historyを更新したのち、(勝手にupdateしてくれる)
+      // onNavigateを実行して、containerに現在のパスが変わったことを伝える
+      // listenで呼び出されることで、引数にhistoryオブジェクトを自動的に渡してくれる
+      onNavigate: ({ pathname: nextPathname }) => {
+        // this is going to be the path that we are currently at inside of our container at.(To prevent infinite loop)
+        const { pathname } = history.location; // old browser history
+        if (pathname !== nextPathname) {
+          // update browser history to url from marketing app inside the container
+          history.push(nextPathname);
+        }
+      },
+    });
+    // container detected history object changed , then execute onParentNavigate
+    history.listen(onParentNavigate);
+    // only try to run this function when our marketing app component is first rendered to the screen.
+  }, []);
 
   return <div ref={ref} />;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history'; // imported history from a library called history not react-rotuer-dom
 import App from './App';
 
 // Mount fn to start up the app
 
-const mount = el => {
-  ReactDOM.render(<App></App>, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  // we then create a memory history object and we tell the memory history object that whenever the URL changes, whenever the path changes , we wanted to automatically call the onNavigate func.
+  if (onNavigate) {
+    // only run this from container app (Not Development)
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      // get current memory history
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        // update container's Browser History by marketing's current memory history
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation,
@@ -14,7 +32,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
